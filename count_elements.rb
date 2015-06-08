79911a0e26b9acd9b996588fe12faca21c28cb0b
chaplonglau <chaplonglau@gmail.com>
2015-06-07 22:04:47 -0700

    tg

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
 def count_elements(array)
   # code goes here
+
+  histogram={}
+
+  array.each do |x|
+    if histogram.has_key?(x)
+      histogram[x]+=1
+    else
+      histogram[x]=1
+    end
+  end
+  return histogram
+
 end
  
