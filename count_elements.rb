2c31228601aa202650e06cc1959d7e252e3d4e39
mendyismyname <m.seldowitz@gmail.com>
2016-03-19 16:54:55 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |item|
+    new_hash[item] ||= 0 
+    new_hash[item] = new_hash[item] + 1
+  end
+  new_hash
 end
  
