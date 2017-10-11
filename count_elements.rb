77c4cc7cbbcd0a7bca294c64a5db9dda89a53a82
iwilliams83 <indirajamurath@gmail.com>
2017-10-11 22:32:28 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
-def count_elements(array)
-  # code goes here
+def count_elements(arr)
+  new_hash = {}
+  comparison = arr.uniq
+  for element in comparison
+    counter = 0
+    arr.each do |x|
+      if x == element
+        counter += 1
+      end
+    end
+    new_hash[element] = counter
+  end
+  return new_hash
 end
- 
